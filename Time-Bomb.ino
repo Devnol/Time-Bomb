28425143422bf39573ae3d3015a60f642629f90b
Devnol <pgkoutsoumanis@gmail.com>
2021-03-24 21:45:23 +0200

    Finalise source code
    Signed-off-by: Devnol <pgkoutsoumanis@gmail.com>

--- Time-Bomb.ino
+++ Time-Bomb.ino
@@ -1,7 +1,3 @@
-#include <LiquidCrystal_I2C.h> // LiquidCrystal_I2C by Frank de Brabander can be found under Tools > Manage Libraries in the Arduino IDE
-#include <CountUpDownTimer.h> // CountUpDownTimer by AndrewMascolo can be found on its GitHub repo: https://github.com/AndrewMascolo/CountUpDownTimer
-#include <Wire.h> // Wire is a built-in Arduino IDE library
-
 //pin assignments, adjust to your needs
 const uint8_t Buzzer = 9; //must support PWM
 const uint8_t LeftBtn = 10;
@@ -11,9 +7,13 @@ const uint8_t RightBtn = 12;
 const uint8_t LcdAddr = 0x3F; //Address of the I2c device, for more info in how to find that see File > Examples > Wire > i2c_scanner in the Arduino IDE
 const uint8_t LcdCols = 16; //Columns of characters your LCD has, usually 16 or 20
 const uint8_t LcdRows = 2; //Rows of characters your LCD has, usually 2 or 4
-uint16_t BuzzFreq = 100; //initial buzzer frequency for the siren sequence
+uint8_t time = 0; //if you don't use buttons replace 0 with a different number (in seconds)
 
-//main code - do not edit unless you know what you're doing
+//DO NOT EDIT BELOW THIS LINE (unless you know what you're doing)
+
+#include <LiquidCrystal_I2C.h> // LiquidCrystal_I2C by Frank de Brabander can be found under Tools > Manage Libraries in the Arduino IDE
+#include <CountUpDownTimer.h> // CountUpDownTimer by AndrewMascolo can be found on its GitHub repo: https://github.com/AndrewMascolo/CountUpDownTimer
+#include <Wire.h> // Wire is a built-in Arduino IDE library
 
 LiquidCrystal_I2C lcd(LcdAddr, LcdCols, LcdRows); //initialise display as lcd
 CountUpDownTimer T(DOWN); //makes a new timer named "T" and sets it to countdown
@@ -26,6 +26,7 @@ void tick(){ //does the beeping sound every second
 }
 
 void siren(){ //does the siren sound after the time is up
+    uint16_t BuzzFreq = 100; //initial buzzer frequency for the siren sequence
     do { //increments the frequency
         BuzzFreq = BuzzFreq + 5;
         tone(Buzzer, BuzzFreq);
@@ -39,40 +40,51 @@ void siren(){ //does the siren sound after the time is up
     } while (BuzzFreq > 100);
 }
 
+uint8_t getTime() {
+    uint8_t setTime = 60;
+    while (!digitalRead(MiddleBtn)) {
+        if (digitalRead(LeftBtn)) {
+            setTime--;
+        } else if (digitalRead(RightBtn)) {
+            setTime++;
+        }
+        lcd.setCursor(0, 0);
+        lcd.print("Set Time: " + String(setTime));
+        lcd.setCursor(0, 1);
+        lcd.print("Seconds");
+        delay(50);
+        lcd.clear();
+    }
+    return setTime;
+}
+
 void setup() { 
-    //initialize lcd screen
-    lcd.init();
-    // turn on the backlight
-    lcd.backlight();
-    tick();
     pinMode(Buzzer,OUTPUT);
     pinMode(LeftBtn,INPUT);
     pinMode(MiddleBtn,INPUT);
     pinMode(RightBtn,INPUT);
-
-    //Time Select code goes here
-    T.SetTimer(10); //set the time the you want the bomb to run for in seconds.
+    lcd.init(); //initialize lcd screen
+    lcd.backlight(); // turn on the backlight
+    if (!time) time = getTime(); // if time hasn't been set by the user, run getTime() to obtain it.
+    T.SetTimer(time); 
     T.StartTimer();       
 }
 
 void loop() {
-   T.Timer();
-   if (T.TimeHasChanged() ){    
-     lcd.setCursor(0, 0); //col, row
-     lcd.print("You have ");
-     lcd.print(T.ShowMinutes());
-     lcd.print(":");
-     lcd.print(T.ShowSeconds());
-     lcd.print(" to");
-     lcd.setCursor(0, 1);
-     lcd.print("clear the area");
-     tick();
-   }
-   else if (T.ShowTotalSeconds() == 0){
-     lcd.clear();
-     lcd.print("Evaquate the");
-     lcd.setCursor(0, 1);
-     lcd.print("building!");
-     siren();
-   }
+    T.Timer();
+    if (T.TimeHasChanged() ){    
+        tick();
+        lcd.setCursor(0, 0); //col, row
+        lcd.print("You have ");
+        lcd.print(T.ShowMinutes());
+        lcd.print(":");
+        lcd.print(T.ShowSeconds());
+        lcd.setCursor(0, 1);
+        lcd.print("to evacuate");
+    }
+    else if (T.ShowTotalSeconds() == 0){
+        lcd.clear();
+        lcd.print("Kaboom lawl");
+        while(true) siren();
+    }
 }
